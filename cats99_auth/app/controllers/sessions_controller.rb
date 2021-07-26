29cc7c3892bc981f6d625e8547bf0771893ee9f6
Hansaem Kim <hansaem_kim@hotmail.com>
2021-07-26 17:33:19 -0400

    fixed login issue

--- cats99_auth/app/controllers/sessions_controller.rb
+++ cats99_auth/app/controllers/sessions_controller.rb
@@ -9,7 +9,6 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
-
         if @user
             login(@user)
             redirect_to cats_url
